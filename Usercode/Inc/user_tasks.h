ff0e88bd89a7902bff3ad9672f4932ac19c1dda1
Airman Han <hiairman@outlook.com>
2025-11-01 12:47:46 +0800

    feat: FreeRTOS配置与Usercode生成

---
+++ Usercode/Inc/user_tasks.h
@@ -0,0 +1,21 @@
+//
+// Created by HiAir on 2025/11/1.
+//
+
+#ifndef JIAOLOONGFINALTASK_USER_TASKS_H
+#define JIAOLOONGFINALTASK_USER_TASKS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void user_task_init();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //JIAOLOONGFINALTASK_USER_TASKS_H
---
+++ Usercode/Src/user_tasks.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HiAir on 2025/11/1.
+//
+
+#include "user_tasks.h"
