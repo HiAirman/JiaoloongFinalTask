182959a9c962d2f19f5529fbffc45f52cb49d8e6
Airman Han <hiairman@outlook.com>
2025-11-01 16:41:16 +0800

    feat: 实现FreeRTOS中的事件标识功能，当send是35倍数时receive++，但是程序存在潜在的bbbbbug

--- Usercode/Src/user_tasks.cpp
+++ Usercode/Src/user_tasks.cpp
@@ -13,9 +13,15 @@
 // };
 
 //Semaphore
-osSemaphoreId_t test_semaphore_handle;
-osSemaphoreAttr_t test_semaphore_attribute{
-    .name = "test_semaphore",
+// osSemaphoreId_t test_semaphore_handle;
+// osSemaphoreAttr_t test_semaphore_attribute{
+//     .name = "test_semaphore",
+// };
+
+//Event Flag
+osEventFlagsId_t test_event_flag_handle;
+osEventFlagsAttr_t test_event_flag_attribute{
+    .name = "test_event_flag"
 };
 
 //tasks
@@ -34,9 +40,15 @@ uint32_t send = 0;
         auto ticks = osKernelGetTickCount();
         //    send++;
         //    osMessageQueuePut(test_queue_handle, &send, osPriorityNormal, 0);
+        // if (send++ % 5 == 0) {
+        //     //预期：send增加5，receive增加1
+        //     osSemaphoreRelease(test_semaphore_handle);
+        // }
         if (send++ % 5 == 0) {
-            //预期：send增加5，receive增加1
-            osSemaphoreRelease(test_semaphore_handle);
+            osEventFlagsSet(test_event_flag_handle, (1u << 0));
+        }
+        if ((send - 1) % 5 == 0) {
+            osEventFlagsClear(test_event_flag_handle, (1u << 0));
         }
         osDelayUntil(ticks + 1);
     }
@@ -49,24 +61,48 @@ osThreadAttr_t test2_task_attribute{
     .priority = osPriorityNormal,
 };
 
+[[noreturn]] void test2_task(void*) {
+    while (true) {
+        auto ticks = osKernelGetTickCount();
+        if (send % 7 == 0) {
+            osEventFlagsSet(test_event_flag_handle, (1u << 1));
+        }
+        if ((send - 1) % 7 == 0) {
+            osEventFlagsClear(test_event_flag_handle, (1u << 1));
+        }
+        osDelayUntil(ticks + 1);
+    }
+}
+
+osThreadId_t test3_task_handle;
+osThreadAttr_t test3_task_attribute{
+    .name = "test3_task",
+    .stack_size = 128 * 4,
+    .priority = osPriorityNormal,
+};
+
 uint32_t receive = 0;
 
-[[noreturn]] void test2_task(void*) {
+[[noreturn]] void test3_task(void*) {
     while (true) {
         //接收
         //    osMessageQueueGet(test_queue_handle, &receive, nullptr, osWaitForever);
-        osSemaphoreAcquire(test_semaphore_handle, osWaitForever);
+        // osSemaphoreAcquire(test_semaphore_handle, osWaitForever);
+        osEventFlagsWait(test_event_flag_handle, (1u << 0) | (1u << 1), osFlagsWaitAll,osWaitForever);
         receive++;
     }
 }
 
-
 void user_task_init() {
     // test_queue_handle = osMessageQueueNew(10, sizeof(uint32_t), &test_queue_attribute);
 
-    test_semaphore_handle = osSemaphoreNew(1, 0, &test_semaphore_attribute);
+    // test_semaphore_handle = osSemaphoreNew(1, 0, &test_semaphore_attribute);
+
+    test_event_flag_handle = osEventFlagsNew(&test_event_flag_attribute);
 
     test_task_handle = osThreadNew(test_task, nullptr, &test_task_attribute);
 
     test2_task_handle = osThreadNew(test2_task, nullptr, &test2_task_attribute);
+
+    test3_task_handle = osThreadNew(test3_task, nullptr, &test3_task_attribute);
 }
