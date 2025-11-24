cbdc1645c05466ec1bf04fc2f143969c5d0ea3c1
Airman Han <hiairman@outlook.com>
2025-11-24 14:46:08 +0800

    feat: setup dbus_isr frame

---
+++ Usercode/Inc/controller.h
@@ -0,0 +1,17 @@
+//
+// Created by HiAir on 2025/11/24.
+//
+
+#ifndef JIAOLOONGFINALTASK_CONTROLLER_H
+#define JIAOLOONGFINALTASK_CONTROLLER_H
+
+
+class Controller {
+public:
+    void dbus_isr_callback();
+};
+
+extern Controller controller;
+
+
+#endif //JIAOLOONGFINALTASK_CONTROLLER_H
